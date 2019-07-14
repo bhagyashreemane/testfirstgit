84af21d0e26e666ba9cfa6062fcdde0ee81a1919
Dell <Dell@192.168.1.11>
2019-07-14 19:52:40 +0530

    Firts project commit

---
+++ ss/src/ss/inherit2.java
@@ -0,0 +1,19 @@
+package ss;
+
+public class inherit2 extends inhertit1 {
+	public void inherit2()
+	{
+		System.out.println("inherti1 parent class");
+	}
+	static int s;
+	static int f;
+	public static void main(String []args)
+	{
+		inherit2 bb=new inherit2();
+	//shov();	
+	int a;
+	
+	//int c=f+s+a;
+	//System.out.println(c);
+	}
+}
---
+++ ss/src/ss/inhertit1.java
@@ -0,0 +1,8 @@
+package ss;
+
+public class inhertit1 {
+public void show()
+{
+	System.out.println("inherti1 parent class");
+}
+}
